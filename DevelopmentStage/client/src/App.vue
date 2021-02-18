34995c5dfe324d09dea5816f659af7f488160370
ashwin97 <ashwix1997@gmail.com>
2021-02-17 23:14:57 -0800

    New view and component, still empty

--- DevelopmentStage/client/src/App.vue
+++ DevelopmentStage/client/src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
     <router-view/>
-    <ProgressBar title="Hi there" progress="15"></ProgressBar>
-    <DescriptiveTag title="Intense" color="red"></DescriptiveTag>
+    <ProgressBar title="Progress Bar" :progress="25"></ProgressBar>
+    <DescriptiveTag title="Complex" color="yellow"></DescriptiveTag>
+    <progress-ring/>
+    <reading-tab title="CRM" readingNumber="9" :isRead="true"></reading-tab>
+    <button v-on:click="calendar">HI</button>
   </div>
 </template>
 
@@ -10,13 +13,26 @@
 
 import ProgressBar from './components/ProgressBar.vue'
 import DescriptiveTag from './components/DescriptiveTag.vue'
+import ProgressRing from './components/ProgressRing.vue'
+import ReadingTab from './components/ReadingTab.vue'
 
+
+var c = require('calendar')
 
 export default {
   name: 'App',
   components: {
     ProgressBar,
-    DescriptiveTag
+    DescriptiveTag,
+    ProgressRing,
+    ReadingTab
+  },
+  methods:{
+    calendar: function(){
+      var cal = new c.Calendar();     
+      var m = cal.monthDays(2021, 1);
+      for (let i=0; i<m.length; i++) console.log(m[i]);  
+    }
   }
 }
 </script>
