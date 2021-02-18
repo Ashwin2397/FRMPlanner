abc17fb3e6517ea61a94950da7c83d5020bc0d91
ashwin97 <ashwix1997@gmail.com>
2021-02-17 23:13:55 -0800

    Add reading tab component

---
+++ DevelopmentStage/client/src/components/ReadingTab.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="grid-container">
+        <div id="content" class="grid-item">
+            {{ title }}, R{{ readingNumber }}
+        </div>
+        <div id="read" class="grid-item">
+            {{ hasRead }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'ReadingTab',
+    props:{
+        title: String,
+        readingNumber: String,
+        isRead: Boolean
+    },
+    computed:{
+        hasRead(){
+            if(this.isRead){
+                return 'X'
+            }
+            
+            return ''
+        }
+    }
+}
+</script>
+
+<style>
+
+
+.grid-container {
+    display: grid;
+    background-color: #2196F3;
+    width: 400px;
+    height: 80px;
+    grid-column-gap: 1px;
+
+}
+.grid-item {
+    display: grid;
+    background-color: rgba(255, 255, 255, 0.8);
+    border: 1px solid rgba(0, 0, 0, 0.8);
+    text-align: center;
+    align-content: center;
+}
+
+#content{
+    grid-column: 1/5;
+}
+
+#read{
+    grid-column: 5/6;
+}
+</style>
